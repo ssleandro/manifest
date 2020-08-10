cc67cff9b164f99740cf7a739abc9b7255cd0c46
Fabio Berton <fabio.berton@ossystems.com.br>
2020-08-10 09:34:23 -0300

    Rework platform to add support to Ambientte product
      - Change to use Inobram Embedded Linux
      - Add support to valid_machine
      - Add support to ambientte-imx7d-r1 machine
      - Add meta-freescale layers
    
    Change-Id: If3350b0536e2651a589ec157994cec8cbd9f5206
    Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

---
+++ setup-environment.d/inobram.py
@@ -0,0 +1,62 @@
+INOBRAM_VALID_MACHINES = []
+
+def __set_defaults_inobram():
+
+    import os
+    import sys
+
+    # Append to valid machines
+    global INOBRAM_VALID_MACHINES
+    INOBRAM_VALID_MACHINES += [
+        'ambientte-imx7d-r1',
+        'smaai5-am335x',
+    ]
+
+    local_conf_exists = os.path.isfile(os.path.join(build_dir,
+                                                    'conf',
+                                                    'local.conf'))
+
+    def required_var_error(varname, valid_vals):
+        sys.stderr.write("ERROR: You must set '%s' before setting up the environment.\n" %
+                         (varname,))
+        sys.stderr.write("       Set MACHINE variable with one of the possible values.\n"
+                         "       Possible values are: %s\n\n"
+                         "       Ex: MACHINE=smaai5-am335x source setup-environment build\n"
+                         % valid_vals)
+        sys.exit(1)
+
+    def maybe_set_default(varname, valid_vals):
+        try:
+            val = os.environ[varname]
+        except KeyError:
+            val = None
+
+        if val:
+            if val in valid_vals:
+                set_default(varname, val)
+            else:
+                required_var_error(varname, valid_vals)
+        elif not local_conf_exists:
+            required_var_error(varname, valid_vals)
+
+    maybe_set_default('MACHINE', INOBRAM_VALID_MACHINES)
+    set_default('DISTRO', 'iel')
+
+def __after_init_inobram_yocto():
+    PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+
+    append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+                    [
+                        'meta-freescale',
+                        'meta-freescale-3rdparty',
+                        'meta-openembedded/meta-networking',
+                        'meta-openembedded/meta-oe',
+                        'meta-openembedded/meta-python',
+                        'meta-qt5',
+                        'meta-smaai5',
+                    ]])
+    # FSL EULA
+    eulas.accept['meta-freescale/EULA'] = 'ACCEPT_FSL_EULA = "1"'
+
+run_set_defaults(__set_defaults_inobram)
+run_after_init(__after_init_inobram_yocto)
